0a081557b9b53704e4c70e0d95873aa9437ca729
Marwane <manbou92@hotmail.fr>
2020-05-10 19:09:41 +0200

    PA-CARSERY

--- www/views/apparence.view.php
+++ www/views/apparence.view.php
@@ -1,7 +1,120 @@
 <div class="container">
     <h1>Apparence</h1>
-    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Illum quaerat autem, cumque ab, exercitationem placeat fugiat, labore dignissimos atque quasi dolor. Doloremque quae dolorum autem vel perspiciatis dignissimos blanditiis quo?</p>
-    <button class="btn">OK</button>
-    <button class="btn btn--danger">OK</button>
-    <button class="btn btn--primary">OK</button>
+    <p>Personnaliser l'apparence de votre dashboard selon vos préférences</p>
+
+    <div class="row">
+        <div class="col-4">
+            <div class="theme">
+                <div class="theme-spec">
+                    <ul>
+                        <li>Sidebar
+                            <div style="background-color: blue; height: 30px; width: 30px;"></div>
+                        </li>
+                        <li>Header
+                            <div style="background-color: green; height: 30px; width: 30px;"></div>
+                        </li>
+                        <li>Footer
+                            <div style="background-color: yellow; height: 30px; width: 30px;"></div>
+                        </li>
+                    </ul>
+                </div>
+                <div class="theme-info">
+                    Par défaut
+                </div>
+            </div>
+        </div>
+        <div class="col-4">
+            <div class="theme">
+                <div class="theme-spec">
+                    <ul>
+                        <li>Sidebar
+                            <div style="background-color: blue; height: 30px; width: 30px;"></div>
+                        </li>
+                        <li>Header
+                            <div style="background-color: green; height: 30px; width: 30px;"></div>
+                        </li>
+                        <li>Footer
+                            <div style="background-color: yellow; height: 30px; width: 30px;"></div>
+                        </li>
+                    </ul>
+                </div>
+                <div class="theme-info">
+                    Thème 2
+                </div>
+            </div>
+        </div>
+        <div class="col-4">
+            <div class="theme">
+                <div class="theme-spec">
+                    <ul>
+                        <li>Sidebar
+                            <div style="background-color: blue; height: 30px; width: 30px;"></div>
+                        </li>
+                        <li>Header
+                            <div style="background-color: green; height: 30px; width: 30px;"></div>
+                        </li>
+                        <li>Footer
+                            <div style="background-color: yellow; height: 30px; width: 30px;"></div>
+                        </li>
+                    </ul>
+                </div>
+                <div class="theme-info">
+                    Thème 3
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="row">
+        <div class="col-4">
+            <div class="theme">
+                <div class="theme-spec">
+                    <ul>
+                        <li>Sidebar
+                            <div style="background-color: blue; height: 30px; width: 30px;"></div>
+                        </li>
+                        <li>Header
+                            <div style="background-color: green; height: 30px; width: 30px;"></div>
+                        </li>
+                        <li>Footer
+                            <div style="background-color: yellow; height: 30px; width: 30px;"></div>
+                        </li>
+                    </ul>
+                </div>
+                <div class="theme-info">
+                    Thème 4
+                </div>
+            </div>
+        </div>
+        <div class="col-4">
+            <div class="theme">
+                <div class="theme-spec">
+                    <ul>
+                        <li>Sidebar
+                            <div style="background-color: blue; height: 30px; width: 30px;"></div>
+                        </li>
+                        <li>Header
+                            <div style="background-color: green; height: 30px; width: 30px;"></div>
+                        </li>
+                        <li>Footer
+                            <div style="background-color: yellow; height: 30px; width: 30px;"></div>
+                        </li>
+                    </ul>
+                </div>
+                <div class="theme-info">
+                    Thème 5
+                </div>
+            </div>
+        </div>
+        <!-- <div class="col-4">
+            <div class="theme">
+                <div class="new-theme">
+                    <img src="../public/img/new.png" alt="New" height="150px" width="150px">
+                </div>
+                <div class="theme-info">
+                    Ajouter un nouveau thème
+                </div>
+            </div>
+        </div> -->
+    </div>
 </div>
