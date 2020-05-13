2ff536a1626d8a0f573a3ba60e4cb215b6309468
Marwane <manbou92@hotmail.fr>
2020-05-13 19:07:43 +0200

    Modification de la page apparence

--- www/views/apparence.view.php
+++ www/views/apparence.view.php
@@ -10,8 +10,53 @@
     }
     */
     ?>
-
     <div class="row">
+        <div class="col-4">
+            <div class="card-theme">
+                <img src="https://pbs.twimg.com/profile_images/791224628140183552/4ij9CwKo_400x400.jpg" alt="Avatar" style="width:100%">
+                <div class="card-theme-info">
+                    <h4>
+                        <b>Par défaut</b>
+                    </h4> 
+                    <p>Les gôuts de l'équipe</p> 
+                </div>
+            </div>
+        </div>
+        <div class="col-4">
+            <div class="card-theme">
+                <img src="https://stickeramoi.com/6737-large_default/autocollant-chiffre-deux.jpg" alt="Avatar" style="width:100%">
+                <div class="card-theme-info">
+                    <h4>
+                        <b>Thème 2</b>
+                    </h4> 
+                    <p>Pour les joyeux</p> 
+                </div>
+            </div>
+        </div>
+        <div class="col-4">
+            <div class="card-theme">
+                <img src="https://files.cults3d.com/uploaders/14518571/illustration-file/1bdff2d0-99e2-4cf9-8518-e1475c538e6e/3_large.png" alt="Avatar" style="width:100%">
+                <div class="card-theme-info">
+                    <h4>
+                        <b>Thème 3</b>
+                    </h4> 
+                    <p>Pour les grincheux</p> 
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <!-- <div class="card-theme">
+        <img src="https://pbs.twimg.com/profile_images/791224628140183552/4ij9CwKo_400x400.jpg" alt="Avatar" style="width:100%">
+        <div class="card-theme-info">
+            <h4>
+                <b>John Doe</b>
+            </h4> 
+            <p>Architect & Engineer</p> 
+        </div>
+    </div> -->
+
+    <!-- <div class="row">
         <div class="col-4">
             <div class="theme">
                 <div class="theme-spec">
@@ -135,5 +180,5 @@
                 </div>
             </div>
         </div>
-    </div>
+    </div> -->
 </div>
