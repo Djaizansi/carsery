f739c5a5e976139531f5a1682d0213a3b836067a
Djaizansi <youcef.jallali@gmail.com>
2020-05-17 00:38:01 +0200

    Ajout de fonction pour intéragir avec le modal (supprimer/ajout de page)

---
+++ www/public/js/scriptss.js
@@ -0,0 +1,63 @@
+function showContent(){
+    document.querySelector('.loader-container').classList.add('hidden');
+}
+setTimeout(showContent, 3000);
+
+
+
+const modalTriggerButtons = document.querySelectorAll("[data-modal-target]");
+const modals = document.querySelectorAll(".modal");
+const modalCloseButtons = document.querySelectorAll(".modal-close");
+
+modalTriggerButtons.forEach(elem => {
+  elem.addEventListener("click", event => toggleModal(event.currentTarget.getAttribute("data-modal-target")));
+});
+modalCloseButtons.forEach(elem => {
+  elem.addEventListener("click", event => toggleModal(event.currentTarget.closest(".modal").id));
+});
+modals.forEach(elem => {
+  elem.addEventListener("click", event => {
+    if(event.currentTarget === event.target) toggleModal(event.currentTarget.id);
+  });
+});
+
+// Maybe also close with "Esc"...
+document.addEventListener("keydown", event => {
+  if(event.keyCode === 27 && document.querySelector(".modal.modal-show")) {
+    toggleModal(document.querySelector(".modal.modal-show").id);
+  }
+});
+
+function toggleModal(modalId) {
+  const modal = document.getElementById(modalId);
+
+  if(getComputedStyle(modal).display==="flex") { // alternatively: if(modal.classList.contains("modal-show"))
+    modal.classList.add("modal-hide");
+    setTimeout(() => {
+      document.body.style.overflow = "initial"; // Optional: in order to enable/disable page scrolling while modal is hidden/shown - in this case: "initial" <=> "visible"
+      modal.classList.remove("modal-show", "modal-hide");
+      modal.style.display = "none";      
+    }, 200);
+  }
+  else {
+    document.body.style.overflow = "hidden"; // Optional: in order to enable/disable page scrolling while modal is hidden/shown
+    modal.style.display = "flex";
+    modal.classList.add("modal-show");
+  }
+}
+
+
+var id_utilisateur_supprimer;
+$(document).on("click", ".myBtn", function () {
+  id_utilisateur_supprimer = $(this).data('id');
+});
+
+$('#btnYes').click(function() {
+	window.location.href = "/supprimer-page?id="+id_utilisateur_supprimer;
+});
+
+$(function(){
+  $('#btnNo').click(function(){
+    $('#modal1').toggleClass('modal-hide')
+  })
+})
