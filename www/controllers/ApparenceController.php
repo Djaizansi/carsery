c957ccd6641d1a138e97d280ad46f28332a52d8f
Marwane <manbou92@hotmail.fr>
2020-05-20 04:22:54 +0200

    Ajout de ApparenceController

--- www/controllers/ApparenceController.php
+++ www/controllers/ApparenceController.php
@@ -6,12 +6,30 @@ use carsery\core\Session;
 use carsery\core\View;
 
 class ApparenceController {
+
     public function apparenceAction()
     {
         if(Session::estConnecte()){
             $myView = new View("apparence");
-        }else {
+        } else {
             include_once "./error/notConnected.php";
+        }
+
+        $theme = "Par défaut";
+        $myView->assign("theme", $theme);
+        
+        if(isset($_POST['themeOne'])){
+            $theme = "Par défaut";
+            $_SESSION['theme'] = $theme;
+            $myView->assign("theme", $theme);
+        } else if(isset($_POST['themeTwo'])){
+            $theme = "Thème 2";
+            $_SESSION['theme'] = $theme;
+            $myView->assign("theme", $theme);
+        } else if(isset($_POST['themeThree'])){
+            $theme = "Thème 3";
+            $_SESSION['theme'] = $theme;
+            $myView->assign("theme", $theme);
         }
     }
 }
