25ac387d6693515ff8aa9ba5185b01ba9e8da1f6
Djaizansi <youcef.jallali@gmail.com>
2020-05-22 00:29:24 +0200

    JsonSerializable

---
+++ www/models/Json.php
@@ -0,0 +1,18 @@
+<?php 
+
+namespace carsery\models;
+
+use JsonSerializable;
+
+class Json implements JsonSerializable {
+    
+    public function __construct(array $array)
+    {
+        $this->array = $array;
+    }
+
+    public function jsonSerialize()
+    {
+        return $this->array;
+    }
+}
