075dc58982b0c95a9f307dbe44c2b1a3690c9397
Djaizansi <youcef.jallali@gmail.com>
2020-05-21 17:56:47 +0200

    Instanciation de BDDException pour la fonction Hydrate

--- www/models/Model.php
+++ www/models/Model.php
@@ -2,6 +2,8 @@
 
 namespace carsery\models;
 
+use carsery\core\Exceptions\BDDException;
+
 class Model
 {
     public function __toArray(): array
@@ -9,31 +11,22 @@ class Model
         return get_object_vars($this);
     }
 
-    /* public function hydrate(array $donnees){
-        $className = get_class($this);
-        $articleObj = new $className();
-        foreach ($donnees as $key => $value){
-        // On récupère le nom du setter correspondant à l'attribut.
-            $method = 'set'.ucfirst($key);
-        // Si le setter correspondant existe bien.
-            if (method_exists($articleObj, $method)){
-            // On appelle le setter.
-            $articleObj->$method($value);
-            }
-        }
-        return $articleObj;
-    } */
-
     public function hydrate(array $donnees){
-        foreach ($donnees as $key => $value){
-        // On récupère le nom du setter correspondant à l'attribut.
-            $method = 'set'.ucfirst($key);
-        // Si le setter correspondant existe bien.
-            if (method_exists($this, $method)){
-                // On appelle le setter.
-                $this->$method($value);
+        if(!empty($donnees)){
+            foreach ($donnees as $key => $value){
+                // On récupère le nom du setter correspondant à l'attribut.
+                $method = 'set'.ucfirst($key);
+                // Si le setter correspondant existe bien.
+                if (method_exists($this, $method)){
+                    // On appelle le setter.
+                    $this->$method($value);
+                }
             }
+        }else {
+            throw new BDDException('Erreur hydratation');
         }
+    
+
         return $this;
     }
 }
