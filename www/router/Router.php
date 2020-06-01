1eb6a7f57c5170c747831c9b25c59d84374aedc7
Djaizansi <youcef.jallali@gmail.com>
2020-06-01 19:06:55 +0200

    Tentative de modification router pour accepter les regex

--- www/router/Router.php
+++ www/router/Router.php
@@ -2,7 +2,6 @@
 
 namespace carsery\router;
 
-use Exception;
 use carsery\core\Exceptions\RouteException;
 
 class Router {
@@ -13,22 +12,26 @@ class Router {
 
     public function routing(){
         $uri = $_SERVER["REQUEST_URI"];
+
         $yaml = yaml_parse_file("router/routes.yml");
         $uri = explode("?",$uri)[0];
+        echo '/myproject/'.$yaml['/myproject/:name']["params"][0]["name"];
+        if(preg_match('/myproject/'.$yaml['/myproject/:name']["params"][0]["name"],$uri)){
+            echo "bjr";
+        }
         if (!empty($yaml[$uri])) {
+
             $c =  'carsery\\controllers\\'.$yaml[$uri]["controller"]."Controller";
             $d = $yaml[$uri]["controller"]."Controller";
             $a =  $yaml[$uri]["action"]."Action";
 
             $pathController = "controllers/".$d.".php";
-
             if (file_exists($pathController)) {
                 include $pathController;
                 //Vérifier que la class existe et si ce n'est pas le cas faites un die("La class controller n'existe pas")
                 if (class_exists($c)) {
                     $controller = new $c();
-
-                    //Vérifier que la méthode existeet si ce n'est pas le cas faites un die("L'action' n'existe pas")
+                    //Vérifier que la méthode existe et si ce n'est pas le cas faites un die("L'action' n'existe pas")
                     if (method_exists($controller, $a)) {
                         //EXEMPLE :
                         //$controller est une instance de la class UserController
