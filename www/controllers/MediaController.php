0103590d1eff52bc41c05043f44f4df6846978b2
Djaizansi <youcef.jallali@gmail.com>
2020-06-03 20:10:42 +0200

    Ajout de la fonctionnalité Media pour upload des images dans un dossier du site

--- www/controllers/MediaController.php
+++ www/controllers/MediaController.php
@@ -11,6 +11,24 @@ class MediaController {
     {
         if(Session::estConnecte()){
             $myView = new View("media");
+            if(isset($_POST['upload'])){
+                foreach($_FILES['file']['name'] as $file){
+                    $file_name[] = $file;
+                    $file_store[] = "./public/images_upload/".$file;
+                }
+
+                foreach($_FILES['file']['tmp_name'] as $file){
+                    $file_tem_loc[] = $file;
+                }
+
+                for ($i = 0; $i < count($_FILES['file']['name']); $i++) {
+                    if(move_uploaded_file($file_tem_loc[$i], $file_store[$i])){
+                        echo "Le fichier a bien été uploadé !";
+                    }else {
+                        echo "Erreur survenue";
+                    }
+                }
+            }
         }else {
             throw new RouteException("Vous devez être connecté");
         }
