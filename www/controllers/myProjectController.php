ec21b4e2e56597ff377252228adec8f8f8ee65b4
Djaizansi <youcef.jallali@gmail.com>
2020-06-08 16:45:46 +0200

    Ajout action QueryBuilder pour tester

--- www/controllers/myProjectController.php
+++ www/controllers/myProjectController.php
@@ -6,6 +6,7 @@ use carsery\core\Exceptions\RouteException;
 use carsery\core\Session;
 use carsery\core\View;
 use carsery\Managers\PageManager;
+use carsery\Managers\PostManager;
 
 class myProjectController {
 
@@ -20,10 +21,21 @@ class myProjectController {
         $public = $found->getPublie();
         if($public == 0 && Session::estConnecte()){
             $myView = new View($notiret,'template1');
+            $myView->assign('found',$found);
         }elseif($public == 1) {
             $myView = new View($notiret,'template1');
         }else {
             throw new RouteException("Il faut être connecter pour modifier la page");
         }
+    }
+
+    public function testQueryBuilderAction()
+    {
+        $postManager = new PostManager();
+
+        $resultTp = $postManager->getUserPost(1);
+
+        echo '<pre>';
+        var_dump($resultTp);
     }
 }
