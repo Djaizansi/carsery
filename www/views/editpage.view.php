b75ddd229581e60fcfea8e43db1ccb3e56e1f2eb
Djaizansi <youcef.jallali@gmail.com>
2020-06-22 15:12:52 +0200

    Test pour visualiser les images

--- www/views/editpage.view.php
+++ www/views/editpage.view.php
@@ -1,8 +1,12 @@
 <?php
+
+use carsery\core\Helpers;
+
 $pageFound = $pageManager->find($_GET['id']);
 $titre = $pageFound->getTitre();
 $notiret = str_replace(' ','',strtolower($titre));
 $file = file_get_contents("Views/$notiret.view.php");
+$files = glob("./public/images_upload/*.*");
 ?>
 
     <div class="container">
