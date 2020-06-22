9a8f91cbeb57940adf48fee44928e642ccd53b7d
Djaizansi <youcef.jallali@gmail.com>
2020-06-22 15:16:57 +0200

    Rename fichier

--- www/public/js/script.js
+++ www/public/js/script.js
@@ -77,27 +77,26 @@ tinymce.init({
   automatic_uploads: true,
   files_picker_types: 'image',
   file_picker_callback: function(cb, value, meta) {
-    var input = document.createElement('input');
-    input.setAttribute('type', 'file');
-    input.setAttribute('accept', 'image/*');
+      var input = document.createElement('input');
+      input.setAttribute('type', 'file');
+      input.setAttribute('accept', 'image/*');
 
-    input.onchange = function() {
+      input.onchange = function() {
       var file = this.files[0];
       var reader = new FileReader();
       
       reader.onload = function () {
-        var id = 'blobid' + (new Date()).getTime();
-        var blobCache =  tinymce.activeEditor.editorUpload.blobCache;
-        var base64 = reader.result.split(',')[1];
-        var blobInfo = blobCache.create(id, file, base64);
-        blobCache.add(blobInfo);
+          var id = 'blobid' + (new Date()).getTime();
+          var blobCache =  tinymce.activeEditor.editorUpload.blobCache;
+          var base64 = reader.result.split(',')[1];
+          var blobInfo = blobCache.create(id, file, base64);
+          blobCache.add(blobInfo);
 
-        // call the callback and populate the Title field with the file name
-        cb(blobInfo.blobUri(), { title: file.name });
+          // call the callback and populate the Title field with the file name
+          cb(blobInfo.blobUri(), { title: file.name });
       };
       reader.readAsDataURL(file);
-    };
-    
-    input.click();
+      };
+      input.click();
   }
-});
+  });
