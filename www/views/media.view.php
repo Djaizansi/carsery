c0ce93317a5142f3346f2a165258f7778ff90132
Djaizansi <youcef.jallali@gmail.com>
2020-07-04 17:24:05 +0200

    Ajout des alertes

--- www/views/media.view.php
+++ www/views/media.view.php
@@ -1,7 +1,6 @@
 <?php
 
 use carsery\core\Helpers;
-
 $files = glob("./public/images_upload/*.*");
 $alert = isset($alert) ? $alert : '';
 ?>
@@ -13,22 +12,26 @@ $alert = isset($alert) ? $alert : '';
 	<?php elseif($alert == "image"): ?>
 		<?= Helpers::alert('danger','',"L'image uploadé n'est pas valide : JPG, PNG, JPEG autorisé") ?>
 		<?php $alert = 0 ?>
+	<?php elseif($_SESSION['success'] === 1): ?>
+		<?= Helpers::alert('success','',"L'image a bien été supprimé") ?>
+		<?php $_SESSION['success'] = ''?>
 	<?php endif ?>
 
     <form action="" method="POST" enctype="multipart/form-data">
         <div class="row">
-            <h2>Ajout d'image dans la bibliothèque</h2>
-            <div class="file-drop-area">
+            <h2 class="txt-center">Ajout d'image dans la bibliothèque</h2>
+            <div style="margin: 0 auto;" class="file-drop-area">
 				<span class="fake-btn"><i class="fas fa-download"></i></span>
 				<span class="file-msg">Faites glisser vos documents ici</span>
 				<input class="file-input" type="file" name="file[]" multiple accept="image/png, image/jpeg, image/jpg">
 			</div>
-            <button class="btn btn--primary" type="submit" name="upload">Enregistrer</button>
-        </div>
+		</div>
+		<br>
+		<button style="display: block; margin: 0 auto;" class="btn btn--primary" type="submit" name="upload">Enregistrer</button>
     </form>
     <br>
-    <h2>Bibliothèque</h2>
-    <table id="myTable" class="display">
+    <h2 class="txt-center">Bibliothèque</h2>
+    <table id="myTable" class="display" style="width: 800px !important; ">
         <thead>
             <th>Image</th>
             <th>Nom</th>
@@ -37,11 +40,13 @@ $alert = isset($alert) ? $alert : '';
         <tbody>
             <?php foreach($files as $file): ?>
             <tr>
-                <td><img src="<?=$file?>" style="width:100px !important; height:100px !important;" alt="Image" /></td>
+                <td>
+					<img src="<?=$file?>" style="width:100px !important; height:100px !important;" alt="Image" />
+				</td>
 				<td><?= pathinfo($file, PATHINFO_FILENAME) ?></td>
 				<td>
-					<button data-modal-target="modal1" data-file="<?=$file?>" class="file" id="myBtn" href="#myBtn"><i class="fas fa-trash-alt"></i></button>
-					<a href="/edit-page?id="><i class="fas fa-edit"></i></a>
+					<?php $newFile = explode('/',$file) ?>
+					<button data-modal-target="modal1" data-file="<?=$newFile[3]?>" class="file" id="myBtn" href="#myBtn"><i class="fas fa-trash-alt"></i></button>
 				</td>
             </tr>
             <?php endforeach ?>
@@ -53,16 +58,8 @@ $alert = isset($alert) ? $alert : '';
         <div class="modal-content"> <!-- This is the actual modal/popup box -->
             <span class="modal-close">&times;</span>
             <p>Souhaitez-vous vraiment supprimer cette Image?</p>
-			<a id="btnYes" class="btn btn--success">Oui</a>
+			<a id="btnYesFile" class="btn btn--success">Oui</a>
 			<a id="btnNo" class="btn btn--danger">Non</a>
-        </div>
-    </div>
-
-    <div class="modal" id="modal2"> <!-- This is the background overlay -->
-        <div class="modal-content"> <!-- This is the actual modal/popup box -->
-            <span class="modal-close">&times;</span>
-            <h2 class="txt-center">Création de page</h2>
-            <?php $this->addModal("form", $configFormPage );?>
         </div>
     </div>
 
