dcc0171e2b81852a0113666bfc14ce5ad6aa02e4
Djaizansi <youcef.jallali@gmail.com>
2020-07-04 17:03:17 +0200

    Modification pour prendre en compte l'ajout du shortcode en base ainsi que la modification de la page

--- www/controllers/PageController.php
+++ www/controllers/PageController.php
@@ -8,8 +8,10 @@ use carsery\core\View;
 use carsery\core\Session;
 use carsery\models\Page;
 use carsery\Managers\PageManager;
+use carsery\Managers\ShortCodeManager;
 use carsery\Managers\UserManager;
 use carsery\models\Json;
+use carsery\models\Shortcode;
 
 class PageController {
 
@@ -28,7 +30,6 @@ class PageController {
             $myView->assign('pageManager',$pageManager);
             $configFormPage = PageManager::getPageForm();
             $myView->assign('configFormPage',$configFormPage);
-
         }else {
             throw new RouteException("Vous devez être connecté");
         }
@@ -43,12 +44,10 @@ class PageController {
             throw new RouteException("La page que vous voulez supprimer n'existe pas ou plus");
         }else {
             $token = $pageFound->getToken();
-            $titre = htmlspecialchars($pageFound->getTitre());
-            $notiret = str_replace(' ','',strtolower($titre));
+
 
             if(isset($_GET['id']) && isset($_GET['token']) && $token == $_GET['token']){
                 $pageManager->delete('id',$_GET['id']);
-                unlink("views/$notiret.view.php");
                 return $this->pageAction();
             }else {
                 throw new RouteException("Vous pouvez pas supprimer d'autre page aussi facilement");
@@ -65,24 +64,19 @@ class PageController {
 
             $findUser = $userManager->find($_SESSION['id']);
             $prenom = $findUser->getFirstname();
+            $titre_tiret = '/myproject/'.str_replace(' ','-',strtolower($_POST['titre']));
+            $pageExist = $pageManager->findByUri($titre_tiret);
             if(!empty($_POST)){
-                $pageExiste = "views/".$_POST['titre'].".view.php";
-                if(file_exists($pageExiste)){
+                if($pageExist){
                     throw new RouteException("La page que vous voulez ajouter existe déjà");
-                }else {
-                    $titre_tiret = '/myproject/'.str_replace(' ','-',strtolower($_POST['titre']));
-                    $menu = isset($_POST['checkbox']) ? 1 : 0;
-                    PageManager::addData($page,$pageManager,'',$_POST['titre'],$prenom,0,$titre_tiret,$menu,$token);
-
-                    $location = Helpers::getUrl('Page','page');
-                    header("Location: $location");
-                    $unePage = $pageManager->findByTitre($_POST['titre']);
-                    if(!empty($unePage)){
-                            $titre = str_replace(' ','',strtolower(htmlspecialchars($unePage->getTitre())));
-                            file_put_contents("views/$titre.view.php",'coucou');
-                        }
-                    }
                 }
+                $unMenu = isset($_POST['checkbox']) ? 1 : 0;
+                $unPublic = isset($_POST['public']) ? 1 : 0;
+                PageManager::addData($page,$pageManager,'',$_POST['titre'],$prenom,'Hello',$unPublic,$titre_tiret,$unMenu,$token);
+
+                $location = Helpers::getUrl('Page','page');
+                header("Location: $location");
+            }
         }else{
             throw new RouteException("Vous devez être connecter pour accèder à cette page");
         }
@@ -100,19 +94,28 @@ class PageController {
     public function modifierPageAction()
     {
         if(Session::estConnecte()){
+            $token = Helpers::Salt(20);
             $pageManager = new PageManager();
+            $page = new Page();
             $configFormPage = PageManager::getWYSIWYGForm();
             $find = $pageManager->findAll();
             if($find && isset($_GET['id'])){
                 $unePage = $pageManager->find($_GET['id']);
+                $auteur = $unePage->getAuteur();
+                $uri = $unePage->getUri();
+                $public = $unePage->getPublie();
+                $menu = $unePage->getMenu();
                 $titre = htmlspecialchars($unePage->getTitre());
-                $notiret = str_replace(' ','',strtolower($titre));
+
                 $myView = new View("editpage");
                 $myView->assign('pageManager', $pageManager);
                 $myView->assign('configFormPage', $configFormPage);
                 if(!empty($_POST)){
                     isset($_POST['editPage']) ? $_POST['editPage'] : '';
-                    file_put_contents("Views/$notiret.view.php", $_POST['editPage']);
+
+                    $unMenu = isset($_POST['checkbox']) ? 1 : 0;
+                    $unPublic = isset($_POST['public']) ? 1 : 0;
+                    PageManager::addData($page,$pageManager,$_GET['id'], $titre,$auteur,$_POST['editPage'],$unPublic,$uri,$unMenu,$token);
                     $location = Helpers::getUrl('Page','page');
                     header("Location: $location");
                 }
@@ -130,22 +133,40 @@ class PageController {
     {
         if(Session::estConnecte()){
             $pageManager = new PageManager();
-            $find = $pageManager->findAll();
+            $page = new Page();
+            $shortCodeManager = new ShortCodeManager();
+            $shortCode = new Shortcode();
+            $find = $pageManager->find($_GET['id']);
             if($find && isset($_GET['id'])){
-                $unePage = $pageManager->find($_GET['id']);
+                /* $unePage = $pageManager->find($_GET['id']);
                 $titre = htmlspecialchars($unePage->getTitre());
-                $notiret = str_replace(' ','',strtolower($titre));
+                $auteur = $unePage->getAuteur();
+                $content = $unePage->getContent();
+                $unPublic = $unePage->getPublie();
+                $uri = $unePage->getUri();
+                $unMenu = $unePage->getMenu(); */
+                /* $token = Helpers::Salt(20); */
+
                 $myView = new View("widget");
+                $myView->assign('shortCodeManager',$shortCodeManager);
+                $myView->assign('shortCode',$shortCode);
                 $myView->assign('pageManager', $pageManager);
+                /* $myView->assign('page', $page);
+                $myView->assign('titre',$titre);
+                $myView->assign('auteur',$auteur);
+                $myView->assign('content',$content);
+                $myView->assign('unPublic',$unPublic);
+                $myView->assign('uri',$uri);
+                $myView->assign('unMenu',$unMenu);
+                $myView->assign('token',$token); */
+
+
                 if(!empty($_POST)){
                     isset($_POST['caroussel']) ? $_POST['caroussel'] : '';
                     isset($_POST['forum']) ? $_POST['forum'] : '';
                     isset($_POST['contact']) ? $_POST['contact'] : '';
                     isset($_POST['vehicule']) ? $_POST['vehicule'] : '';
                     isset($_POST['piece']) ? $_POST['piece'] : '';
-                    /* file_put_contents("Views/$notiret.view.php", $_POST['editPage']); */
-                    /* $location = Helpers::getUrl('Page','page');
-                    header("Location: $location"); */
                 }
             }elseif(!$find && !isset($_GET['id'])){
                 throw new RouteException("La modification n'est pas disponible ");
