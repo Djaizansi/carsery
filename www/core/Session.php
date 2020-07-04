00782e8631e013afc75610f11399ba361b66fa2f
Djaizansi <youcef.jallali@gmail.com>
2020-07-04 17:11:49 +0200

    Ajout de la fonction estAdmin()

--- www/core/Session.php
+++ www/core/Session.php
@@ -2,6 +2,8 @@
 
 namespace carsery\core;
 
+use carsery\Managers\UserManager;
+
 session_start();
 class Session {
 
@@ -11,6 +13,13 @@ class Session {
 
     public static function estConnecte() {
         return isset($_SESSION["id"]);
+    }
+
+    public static function estAdmin() {
+        $userManager = new UserManager();
+        $foundUser = $userManager->find($_SESSION['id']);
+        $role = $foundUser->getStatus();
+        if($role === "Admin") return true;
     }
 
     public function deconnecter() 
