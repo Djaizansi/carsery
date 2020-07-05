f204d7601b13b8ef0712a78aaa62202bca1f4756
nesnes9 <nesrinetebaili_9@outlook.fr>
2020-07-05 03:48:22 +0200

    Ajout du manager des objets message

---
+++ www/Managers/MessageManager.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace carsery\Managers;
+
+
+use carsery\core\DB;
+use carsery\models\Message;
+
+class MessageManager extends DB
+{
+    public function __construct()
+    {
+        parent::__construct(Message::class, 'message');
+    }
+
+    /**
+     * Formulaire d'ajout et de modification des messages
+     * @param null $message
+     * @return array
+     */
+    public static function getMessageForm($message = null)
+    {
+
+        $action = (isset($message)) ? helpers::getUrl("Forum", "updatemessagearticle") : helpers::getUrl("Forum", "addmessagearticle");
+        $submit = (isset($message)) ? 'Modifier' : 'Ajouter';
+        $messValue = (isset($message)) ? ["value" => $message->getMessage()] : [];
+        $idValue = (isset($message)) ? ["id" => ["value" => $message->getId(), "type" => "hidden"]] : [];
+
+        return [
+            "config" => [
+                "method" => "POST",
+                "action" => $action,
+                "class" => "box",
+                "id" => "formAddArticle",
+                "submit" => $submit
+            ],
+
+            "fields" => array_merge([
+                "message" => array_merge([
+                    "balise" => "textarea",
+                    "type" => "text",
+                    "placeholder" => "Message",
+                    "id" => "",
+                    "class" => "box",
+                    "required" => true,
+                    "min-lenght" => 2,
+                    "value" => "",
+                    "errorMsg" => "Veuillez renseigner un message"
+                ], $messValue),
+            ], $idValue)
+        ];
+    }
+
+
+}
