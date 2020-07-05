1206aa569a5763a418f0492ae03daa1501a7263b
Djaizansi <youcef.jallali@gmail.com>
2020-07-05 18:17:15 +0200

    Modification de la page lien en widget

--- www/views/templates/back.tpl.php
+++ www/views/templates/back.tpl.php
@@ -7,7 +7,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Dashboard</title>
     <!-- Framework CSS -->
-    <link rel="stylesheet" href="../public/dist/mains.css">
+    <link rel="stylesheet" href="../public/dist/main.css">
     <!-- Dashboard CSS -->
     <link rel="stylesheet" href="../public/css/dashboards.css">
     <!-- Loader CSS -->
@@ -28,6 +28,8 @@
     <!-- SCRIPTJS -->
     <script src="../public/js/user.js"></script>
     <!-- WYSIWYG -->
+    <script type="text/javascript" src="https://studio-42.github.io/elFinder/demo/js/elfinder.min.js"></script>
+    <script type="text/javascript" src="https://nao-pon.github.io/tinymceElfinder/tinymceElfinder.js"></script>
     <script src="https://cdn.tiny.cloud/1/vavj4oxk7dimm3sd4lw8lfnr7nk56akfvv84yc0jxo0m0cgz/tinymce/5/tinymce.min.js" referrerpolicy="origin"></script>
     
 </head>
@@ -61,7 +63,7 @@
                 <li class="sidenav__list-item"><a href="<?= Helpers::getUrl("Page","page")?>"><i class="far fa-file"></i>Pages<i class="fas fa-chevron-right"></i></a></li>
                 <li class="sidenav__list-item"><a href="<?= Helpers::getUrl("Forum","forum")?>"><i class="far fa-comments"></i>Forum<i class="fas fa-chevron-right"></i></a></li>
                 <li class="sidenav__list-item"><a href="<?= Helpers::getUrl("Media","media")?>"><i class="fas fa-camera"></i>Médias<i class="fas fa-chevron-right"></i></a></li>
-                <li class="sidenav__list-item"><a href="<?= Helpers::getUrl("Page","lien")?>"><i class="fas fa-link"></i>Liens<i class="fas fa-chevron-right"></i></a></li>
+                <li class="sidenav__list-item"><a href="<?= Helpers::getUrl("Page","widgetPage")?>"><i class="fas fa-box"></i>Widget<i class="fas fa-chevron-right"></i></a></li>
                 <hr>
                 <li class="sidenav__list-item"><a href="<?= Helpers::getUrl("Apparence","apparence")?>"><i class="fas fa-palette"></i>Apparence<i class="fas fa-chevron-right"></i></a></li>
                 <li class="sidenav__list-item"><a href="<?= Helpers::getUrl("User","gestionuser")?>"><i class="fas fa-user"></i>Utilisateurs<i class="fas fa-chevron-right"></i></a></li>
@@ -82,6 +84,6 @@
 
     </div>
     <script src="../public/js/menu.js"></script>
-    <script src="../public/js/script.js"></script>
+    <script src="../public/js/scripts.js"></script>
 </body>
 </html>
