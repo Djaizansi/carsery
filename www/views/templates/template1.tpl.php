13a1483bf6c217327da4d2b3134872d02088968c
Djaizansi <youcef.jallali@gmail.com>
2020-07-07 11:44:50 +0200

    Rendu des liens dynamique + remplacement ancien template par template design by Marwane

--- www/views/templates/template1.tpl.php
+++ www/views/templates/template1.tpl.php
@@ -7,87 +7,114 @@ use carsery\Managers\PageManager;
 $pageManager = new PageManager();
 $foundPage = $pageManager->findAll();
 $foundMyPage = $pageManager->findByUri($_SERVER['REQUEST_URI']);
-?>
-
-
+?>    
 
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="../public/dist/mains.css">
-    <link rel="stylesheet" href="../public/css/template1.css">
-    <link rel="stylesheet" href="../public/css/slider.css">
-    <script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
-    <script src="../public/js/template1.js"></script>
-    <script src="../public/js/slider.js"></script>
-    <title>MyProject</title>
-</head>
-<body>
-    <header>
-            <?php if($foundMyPage): ?>
-                <?php if($foundMyPage->getPublie() == 0): ?>
-                    <a style="float: left;"href="/page">Retour Accueil</a>
-                <?php endif ?>
-            <?php endif ?>
-        <div class="container clearfix">
+    <head>
+        <meta charset="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <title>MyProject</title>
+        <!-- Framework CSS -->
+        <link rel="stylesheet" href="../public/dist/mains.css" />
+        <link rel="stylesheet" href="../public/css/template1.css" />
+        <link rel="stylesheet" href="../public/css/slider.css" />
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" />
+        <script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
+        <script src="../public/js/slider.js"></script>
+    </head>
+    <body>
+        <div class="wrapper">
+            <header class="header">
+                <?php //if($foundMyPage): ?>
+                    <?php if(Session::estAdmin()): ?>
+                        <a style="float: left;"href="<?=Helpers::getUrl('Page','page')?>">Retour Accueil</a>
+                    <?php elseif(empty($_SESSION['id']) || Session::estClient()): ?>
+
+                    <?php endif ?>
+                <?php //endif ?>
+
             <?php foreach($foundPage as $unePage): ?>
                 <?php if($unePage->getHome() == 1): ?>
                     <?php $myPage = 1 ?>
-                    <a href="<?= Helpers::getUrl("myProject","view")?>" class="logo" style="text-decoration: none;">
-                        <p style="color: #000000;">MyProject</p>
-                    </a>
+                    <a href="<?= Helpers::getUrl("myProject","view")?>" class="logo" style="text-decoration: none;">MyProject</a>
                     <?php break; ?>
                 <?php elseif($unePage->getHome() == 0): ?>
                     <?php $myPage = 0 ?>
                 <?php endif ?>
             <?php endforeach ?>
-
+            
             <?php if($myPage == 0):?>
-                <a href="#" class="logo" style="text-decoration: none;">
-                    <p style="color: #000000;">MyProject</p>
-                </a>
-            <?php else: ?>
-                
+                <a href="#" class="logo">MyProject</a>
+            <?php else: ?>     
             <?php endif ?>
-            <nav>
-                <ul>
-                    <?php foreach($foundPage as $unePage): ?>
-                        <?php if($unePage->getMenu() == 1): ?>
-                            <li><a href="<?= $unePage->getUri() ?>"> <?= ucfirst($unePage->getTitre()) ?> </a></li>
-                        <?php endif ?>
-                    <?php endforeach ?>
-                    <?php if(!Session::estConnecte()): ?>
-                        <li style="margin-top: 23px;">|</li>
-                        <li><a href="<?= Helpers::getUrl("User","login")?>">Connexion</a></li>
-                        <li><a href="<?= Helpers::getUrl("User","register")?>">Inscription</a></li>
-                    <?php elseif(Session::estClient()): ?>
-                        <li><a href="#">Panier</a></li>
-                        <li style="margin-top: 23px;">|</li>
-                        <li><a href="<?= Helpers::getUrl("User","deconnecter")?>">Deconnexion</i></a></li>
+            
+            <div class="header-right">
+                <?php foreach($foundPage as $unePage): ?>
+                    <?php $classe = ($_SERVER['REQUEST_URI'] === $unePage->getUri()) ? 'active' : '' ?>
+                    <?php if($unePage->getMenu() == 1): ?>
+                        <a class="<?=$classe?>" href="<?= $unePage->getUri() ?>"> <?= ucfirst($unePage->getTitre()) ?> </a>
                     <?php endif ?>
-                </ul>
-            </nav>
+                <?php endforeach ?>
+                <?php if(!Session::estConnecte()): ?>
+                    <a href="<?= Helpers::getUrl("User","login")?>">Connexion</a></li>
+                    <a  href="<?= Helpers::getUrl("User","register")?>">Inscription</a></li>
+                <?php elseif(Session::estClient()): ?>
+                    <?php $classe = ($_SERVER['REQUEST_URI'] === $unePage->getUri()) ? 'active' : '' ?>
+                    <a class="<?=$classe?>" href="#">Panier</a></li>
+                    <a href="<?= Helpers::getUrl("User","deconnecter")?>">Deconnexion</i></a></li>
+                <?php endif ?>
+            </div>
+            </header>
+            <main>
+                <div class="container">
+                    <?php include "views/".$this->view.".view.php"; ?>
+                    <!-- <div class="row">
+                        <div class="col-6 txt-center">
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate
+                            quidem enim facilis similique necessitatibus eaque officia corporis,
+                            maiores deserunt dolorem, voluptate a voluptatum. Voluptates quasi
+                            placeat voluptatum adipisci animi corporis?
+                        </div>
+                        <div class="col-6 txt-center">
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate
+                            quidem enim facilis similique necessitatibus eaque officia corporis,
+                            maiores deserunt dolorem, voluptate a voluptatum. Voluptates quasi
+                            placeat voluptatum adipisci animi corporis?
+                        </div>
+                    </div> -->
+                </div>
+            </main>
         </div>
-    </header>
 
-        <?php include "views/".$this->view.".view.php"; ?>
+        <footer class="site-footer">
+            <div class="container">
+                <div class="row">
+                    <div class="col-12">
+                    <h3>About</h3>
+                    <p>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                        Deserunt perferendis, sit libero saepe eos omnis sed magnam
+                        incidunt fuga, ex, recusandae distinctio voluptatibus dolor
+                        cupiditate unde quos optio minus voluptatum.
+                    </p>
+                    </div>
 
-    <footer style="bottom: 0;width:100%;">
-        <div class="container clearfix">
-            <section>
-                <nav>
-                    <ul>
-                        <li><a href="#">Légal</a></li>
-                        <li><a href="#">Cookies</a></li>
-                        <li><a href="#">A propos des pubs</a></li>
-                    </ul>
-                </nav>
-            </section>
-        </div>   
-    </footer>
+                    <hr />
+
+                    <div class="txt-center">
+                    <!-- Add font awesome icons -->
+                    <a href="#" class="fa fa-facebook"></a>
+                    <a href="#" class="fa fa-twitter"></a>
+                    <a href="#" class="fa fa-instagram"></a>
+                    <a href="#" class="fa fa-snapchat-ghost"></a>
+                    <a href="#" class="fa fa-pinterest"></a>
+                    <a href="#" class="fa fa-google"></a>
+                    <a href="#" class="fa fa-youtube"></a>
+                    </div>
+                </div>
+            </div>
+</footer>
+
     </body>
 </html>
-    
