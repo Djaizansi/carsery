9cae3d009f449f513dc145ca2f71d92b2e0bc6dc
Djaizansi <youcef.jallali@gmail.com>
2020-07-07 11:50:16 +0200

    Ajout de widget by shortcode

--- www/views/widget.view.php
+++ www/views/widget.view.php
@@ -1,12 +1,11 @@
 <?php
 use carsery\core\Helpers;
-use carsery\Managers\PageManager;
 $i = 1;
 $files = glob("./public/images_upload/*.*");
 $choix = [];
 ?>
 
-<div class="container">
+        <div class="container">
             <h2>Ajout de widget</h2>
             <form action="" method="POST">
                 <label>Caroussel</label>
@@ -39,7 +38,7 @@ $choix = [];
                         </tbody>
                     </table>
                     <label>Nom du shortcode: </label>
-                    <input type="text" name="namecarousel">
+                    <input type="text" name="namecarousel" required>
                     <br><br><br>
                 </div>
 
@@ -61,19 +60,20 @@ $choix = [];
                 
                 <br><br>
                 <button type="submit" class="btn btn--primary">Ajout Widget</button>
+                <br><br>
             </form>
-            <?php if(isset($_POST['caroussel']) && $_POST['caroussel'] == "caroussel"): ?>
-                <?php $content = ''; ?>
-                <?php $data = array_fill_keys(array('listOfPictures'),$choix) ?>
-                <?php $newData = '$data = ' . var_export($data, true) . '; ' ?>
-                <?php $caroussel = ' $this->addModal("carousel", $data); ' ?>
-
-                <?php $content.= $newData ?>
-                <?php $content.= $caroussel ?> 
-
-
-                <?php $shortCode->setShortCode('['.$_POST['namecarousel'].']'); ?>
-                <?php $shortCode->setContent($content); ?>
+            <?php if(isset($_POST['caroussel'])): ?>
+                <?php $data = implode(',',$choix) ?>
+                <?php if(empty($_POST['namecarousel'])): ?>
+                    <?php $errors[] = "Vous devez renseigner le nom de votre shortcode" ?>
+                    <?php if(!empty($errors)): ?>
+                        <?= Helpers::alert('danger',$errors) ?>
+                    <?php endif ?>
+                <?php else: ?>
+                    <?php $shortCode->setShortCode('['.$_POST['namecarousel'].']'); ?>
+                <?php endif ?>
+                <?php $shortCode->setImages($data); ?>
+                <?php $shortCode->setType('caroussel'); ?>
                 <?php $shortCodeManager->save($shortCode); ?>
             <?php endif ?>
     </div>
