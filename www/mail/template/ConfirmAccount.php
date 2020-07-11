24bf24713b22c5a7a2326e062f133a716b307705
Marwane <manbou92@hotmail.fr>
2020-07-11 14:46:41 +0200

    Ajout de la confirmation par mail

---
+++ www/mail/template/ConfirmAccount.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace carsery\mail\template;
+
+use carsery\core\Helpers;
+use carsery\Managers\UserManager;
+
+class ConfirmAccount
+{
+    public static function mailConfirm($pseudo, $email)
+    {
+        $userManager = new UserManager();
+        $found = $userManager->findByEmail($email);
+        var_dump($_POST);
+        $id = $found->getId();
+        $accueil = Helpers::getUrl('myProject', 'view');
+        $confirmation = Helpers::getUrl('User', 'confirmAccount');
+        $token = $found->getToken();
+        return '<!DOCTYPE html>
+                <html lang="en">
+                <head>
+                    <meta charset="UTF-8">
+                    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+                    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+                    <title>Confirmation de compte - Carsery</title>
+                </head>
+                <body>
+                    <font color="#303030";>
+                     <div align="center">
+                        <table width="600px">
+                            <tr>
+                                <td>
+                                    <div align="center">
+                                        Bonjour <b>' . $pseudo . '</b>,<br>
+                                        Voici votre lien d\'activation de compte: <a href="http://151.80.149.232/confirmAccount?id=' . $id . '&token=' . $token . '">Activer mon compte</a> <br>
+                                        A bientôt sur <a href="http://151.80.149.232">Votre site</a> !
+                                    </div>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td align="center">
+                                    <font size="2">
+                                         Ceci est un email automatique, merci de ne pas y répondre
+                                    </font>
+                                </td>
+                            </tr>
+                        </table>
+                    </div>
+                    </font>
+                </body>
+        </html>';
+    }
+}
