d0674669eec242688eec2e9e90573d912b50a938
Marwane <manbou92@hotmail.fr>
2020-07-11 16:24:08 +0200

    Constantes d'installation

---
+++ www/conf.inc.php
@@ -0,0 +1,8 @@
+<?php
+        define("DBDRIVER", "mysql");
+        define("DBHOST", "database");
+        define("DBNAME", "carsery");
+        define("DBUSER", "carsery");
+        define("DBPWD", "root");
+        define("WEBSITE_TITLE", "test");             
+        define("DESCRIPTION", "test");
