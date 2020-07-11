bf7949b256e1a9b78dc74e7d56f8a3aaf4bf27f3
Marwane <manbou92@hotmail.fr>
2020-07-11 16:02:15 +0200

    Récupération des thèmes dans la template

--- www/views/apparence.view.php
+++ www/views/apparence.view.php
@@ -3,105 +3,23 @@
     <h1>Apparence</h1>
     <p>Personnaliser l'apparence de votre dashboard selon vos préférences</p>
 
-    <div class="col-3 txt-center">
-        <p class="alert--primary">
-            
-            Thème actuel : 
-            
-            <?php 
-            
-            if(isset($_SESSION['theme'])){ 
-                echo $_SESSION['theme'];
-            } else {
-                echo "Par défaut";
-            }
-                
-            ?>
-        </p>
-    </div>
-
-    
-    <div class="row">
-        <div class="col-12">
-
-        <?php if($theme !== "Par défaut") { ?>
-
-        <form action="" method="post">
-            <input type="hidden" name="themeOne" value="<?php echo $theme; ?>">
-            <input type="submit" class="btn" value="Séléctionner le thème 1">
-        </form>
-
-        <?php } ?>
-
-        <?php
-        
-        if(isset($theme)){
-            if($theme !== "Thème 2") {
-        ?>
-
-        <form action="" method="post">
-            <input type="hidden" name="themeTwo" value="<?php echo $theme; ?>">
-            <input type="submit" class="btn btn--primary" value="Séléctionner le thème 2">
-        </form>
-
-        <?php } ?>
-
-        <?php
-            if($theme !== "Thème 3") {
-        ?>
-            <form action="" method="post">
-                <input type="hidden" name="themeThree" value="<?php echo $theme; ?>">
-                <input type="submit" class="btn btn--success" value="Séléctionner le thème 3">
-            </form>
-        <?php 
-            }
-        } 
-        ?>
-        </div>
-    </div>
-
     <div class="row">
 
-        <div class="col-4">
-            <div class="card-theme">
-                <img src="../public/img/default_theme.jpg" alt="Avatar" style="width:100%">
-                <div class="card-theme-info">
-                    <h4>
-                        <b>Par défaut</b>
-                    </h4> 
-                    <p>Les gôuts de l'équipe</p> 
+        <?php foreach ($foundTheme as $theme) { ?>
+            <div class="col-4">
+                <div class="card-theme">
+                    <img src="../public/img/theme.jpg" alt="Thème" style="width:100%">
+                    <div class="card-theme-info">
+                        <h4>
+                            <b><?php echo $theme->getTitle(); ?></b>
+                        </h4>
+                        <p><?php echo $theme->getContent(); ?></p>
+                    </div>
                 </div>
             </div>
-        </div>
-
-
-        <div class="col-4">
-            <div class="card-theme">
-                <img src="../public/img/theme_2.jpg" alt="Avatar" style="width:100%">
-                <div class="card-theme-info">
-                    <h4>
-                        <b>Thème 2</b>
-                    </h4> 
-                    <p>Pour les joyeux</p>
-                </div>
-            </div>
-        </div>
-
-
-        <div class="col-4">
-            <div class="card-theme">
-                <img src="../public/img/theme_3.png" alt="Avatar" style="width:100%">
-                <div class="card-theme-info">
-                    <h4>
-                        <b>Thème 3</b>
-                    </h4> 
-                    <p>Pour les grincheux</p> 
-                </div>
-            </div>
-        </div>
-
+        <?php
+        }
+        ?>
 
     </div>
 </div>
-
-<script src="../public/js/modal_newv.js"></script>
