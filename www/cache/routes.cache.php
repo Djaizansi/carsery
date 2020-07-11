9f1ff178e349c75485011bd7bb8d37bc545daad6
Marwane <manbou92@hotmail.fr>
2020-07-11 16:42:26 +0200

    Modification du cache

--- www/cache/routes.cache.php
+++ www/cache/routes.cache.php
@@ -1,42 +1,112 @@
 <?php return array(
   '/' =>
   array(
-    'controller' => 'default',
-    'action' => 'default',
+    'controller' => 'User',
+    'action' => 'login',
+  ),
+  '/deconnexion' =>
+  array(
+    'controller' => 'User',
+    'action' => 'deconnecter',
+  ),
+  '/dashboard' =>
+  array(
+    'controller' => 'Dashboard',
+    'action' => 'dashboard',
+  ),
+  '/mailbox' =>
+  array(
+    'controller' => 'Mail',
+    'action' => 'mail',
+  ),
+  '/apparence' =>
+  array(
+    'controller' => 'Apparence',
+    'action' => 'apparence',
+  ),
+  '/forum' =>
+  array(
+    'controller' => 'Forum',
+    'action' => 'forum',
+  ),
+  '/media' =>
+  array(
+    'controller' => 'Media',
+    'action' => 'media',
+  ),
+  '/page' =>
+  array(
+    'controller' => 'Page',
+    'action' => 'page',
+  ),
+  '/lien' =>
+  array(
+    'controller' => 'Page',
+    'action' => 'lien',
+  ),
+  '/parametre' =>
+  array(
+    'controller' => 'Parametre',
+    'action' => 'parametre',
+  ),
+  '/gestionuser' =>
+  array(
+    'controller' => 'User',
+    'action' => 'gestionuser',
   ),
   '/ajout-utilisateur' =>
   array(
-    'controller' => 'user',
+    'controller' => 'User',
     'action' => 'add',
+  ),
+  '/recupcode' =>
+  array(
+    'controller' => 'User',
+    'action' => 'recupcode',
+  ),
+  '/changemdp' =>
+  array(
+    'controller' => 'User',
+    'action' => 'changemdp',
   ),
   '/supprimer-utilisateur' =>
   array(
-    'controller' => 'user',
+    'controller' => 'User',
     'action' => 'remove',
   ),
   '/utilisateur' =>
   array(
-    'controller' => 'user',
+    'controller' => 'User',
     'action' => 'default',
-  ),
-  '/test/user' =>
-  array(
-    'controller' => 'default',
-    'action' => 'default',
-  ),
-  '/connexion' =>
-  array(
-    'controller' => 'user',
-    'action' => 'login',
   ),
   '/inscription' =>
   array(
-    'controller' => 'user',
+    'controller' => 'User',
     'action' => 'register',
   ),
   '/mdpoublie' =>
   array(
-    'controller' => 'user',
+    'controller' => 'User',
     'action' => 'forget',
+  ),
+  '/ajouter-page' =>
+  array(
+    'controller' => 'Page',
+    'action' => 'addPage',
+  ),
+  '/supprimer-page' =>
+  array(
+    'controller' => 'Page',
+    'action' => 'deletePage',
+  ),
+  '/edit-page' =>
+  array(
+    'controller' => 'Page',
+    'action' => 'modifierPage',
+  ),
+  '/json' =>
+  array(
+    'controller' => 'Page',
+    'action' => 'json',
   ),
 );
