1d81da45f9cf048b94ed33ed53ecb1803e412c2c
Marwane <manbou92@hotmail.fr>
2020-07-11 15:41:59 +0200

    Ajout de ThemeManager pour récuperer les thèmes

--- www/controllers/ApparenceController.php
+++ www/controllers/ApparenceController.php
@@ -1,34 +1,25 @@
-<?php 
+<?php
 
 namespace carsery\controllers;
 
+use carsery\core\Exceptions\RouteException;
 use carsery\core\Session;
 use carsery\core\View;
+use carsery\Managers\ThemeManager;
 
-class ApparenceController {
+class ApparenceController
+{
     public function apparenceAction()
     {
-        if(Session::estConnecte()){
+        if (Session::estConnecte()) {
             $myView = new View("apparence");
-        } else {
-            include_once "./error/notConnected.php";
-        }
 
-        $theme = "Par défaut";
-        $myView->assign("theme", $theme);
-        
-        if(isset($_POST['themeOne'])){
-            $theme = "Par défaut";
-            $_SESSION['theme'] = $theme;
-            $myView->assign("theme", $theme);
-        } else if(isset($_POST['themeTwo'])){
-            $theme = "Thème 2";
-            $_SESSION['theme'] = $theme;
-            $myView->assign("theme", $theme);
-        } else if(isset($_POST['themeThree'])){
-            $theme = "Thème 3";
-            $_SESSION['theme'] = $theme;
-            $myView->assign("theme", $theme);
+            // Affichage des thèmes disponibles
+            $themeManager = new ThemeManager();
+            $foundTheme = $themeManager->findAll();
+            $myView->assign("foundTheme", $foundTheme);
+        } else {
+            throw new RouteException("Vous devez être connecté");
         }
     }
 }
