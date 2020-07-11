955f141f5078edc6b303b11b08d32e1002db2273
Marwane <manbou92@hotmail.fr>
2020-07-11 15:54:31 +0200

    Modification de la template Back

--- www/views/templates/back.tpl.php
+++ www/views/templates/back.tpl.php
@@ -1,7 +1,10 @@
-<?php use carsery\core\Helpers; ?>
+<?php
+
+use carsery\core\Helpers; ?>
 
 <!DOCTYPE html>
 <html lang="fr">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -10,10 +13,9 @@
     <link rel="stylesheet" href="../public/dist/mains.css">
     <!-- Dashboard CSS -->
     <link rel="stylesheet" href="../public/css/dashboards.css">
-    <!-- Thème 2 -->
-    <!-- <link rel="stylesheet" href="../public/css/joyeux.css"> -->
     <!-- Loader CSS -->
     <link rel="stylesheet" href="../public/css/loader.css">
+    <link rel="stylesheet" href="../public/css/dragdrop.css">
     <!-- FontAwesome -->
     <script src="https://kit.fontawesome.com/42ce300797.js" crossorigin="anonymous"></script>
     <!-- JQuery -->
@@ -28,17 +30,21 @@
     <link rel="icon" href="../public/img/carsery.png">
     <!-- SCRIPTJS -->
     <script src="../public/js/user.js"></script>
+    <!-- WYSIWYG -->
+    <script src="https://cdn.tiny.cloud/1/vavj4oxk7dimm3sd4lw8lfnr7nk56akfvv84yc0jxo0m0cgz/tinymce/5/tinymce.min.js" referrerpolicy="origin"></script>
+
 </head>
+
 <body>
 
     <div class="grid-container">
         <div class="menu-icon">
             <i class="fas fa-bars header__menu"></i>
         </div>
-    
+
         <header class="header">
             <div class="header__search">Bienvenue sur votre dashboard</div>
-            <div class="header__avatar"><a href="<?= Helpers::getUrl("User","deconnecter")?>" style="text-decoration: none; color: #fff;"><i class="fas fa-sign-in-alt" style="padding-right: 10px"></i>Quitter</a></div>
+            <div class="header__avatar"><a href="<?= Helpers::getUrl("User", "deconnecter") ?>" style="text-decoration: none; color: #fff;">Déconnexion</a></div>
         </header>
 
         <aside class="sidenav">
@@ -55,22 +61,22 @@
                         <p class="user-txt">Bonjour, <?= $firstname ?></p>
                     </div>
                 </li>
-                <li class="sidenav__list-item"><a href="<?= Helpers::getUrl("Dashboard","dashboard")?>"><i class="fas fa-tachometer-alt"></i>Dashboard</a></li>
-                <li class="sidenav__list-item"><a href="<?= Helpers::getUrl("Page","page")?>"><i class="far fa-file"></i>Pages<i class="fas fa-chevron-right"></i></a></li>
-                <li class="sidenav__list-item"><a href="<?= Helpers::getUrl("Forum","forum")?>"><i class="far fa-comments"></i>Forum<i class="fas fa-chevron-right"></i></a></li>
-                <li class="sidenav__list-item"><a href="<?= Helpers::getUrl("Media","media")?>"><i class="fas fa-camera"></i>Médias<i class="fas fa-chevron-right"></i></a></li>
-                <li class="sidenav__list-item"><a href="<?= Helpers::getUrl("Page","lien")?>"><i class="fas fa-link"></i>Liens<i class="fas fa-chevron-right"></i></a></li>
+                <li class="sidenav__list-item"><a href="<?= Helpers::getUrl("Dashboard", "dashboard") ?>"><i class="fas fa-tachometer-alt"></i>Dashboard</a></li>
+                <li class="sidenav__list-item"><a href="<?= Helpers::getUrl("Page", "page") ?>"><i class="far fa-file"></i>Pages<i class="fas fa-chevron-right"></i></a></li>
+                <li class="sidenav__list-item"><a href="<?= Helpers::getUrl("Forum", "forum") ?>"><i class="far fa-comments"></i>Forum<i class="fas fa-chevron-right"></i></a></li>
+                <li class="sidenav__list-item"><a href="<?= Helpers::getUrl("Media", "media") ?>"><i class="fas fa-camera"></i>Médias<i class="fas fa-chevron-right"></i></a></li>
+                <li class="sidenav__list-item"><a href="<?= Helpers::getUrl("Page", "widgetPage") ?>"><i class="fas fa-box"></i>Widget<i class="fas fa-chevron-right"></i></a></li>
                 <hr>
-                <li class="sidenav__list-item"><a href="<?= Helpers::getUrl("Apparence","apparence")?>"><i class="fas fa-palette"></i>Apparence<i class="fas fa-chevron-right"></i></a></li>
-                <li class="sidenav__list-item"><a href="<?= Helpers::getUrl("User","gestionuser")?>"><i class="fas fa-user"></i>Utilisateurs<i class="fas fa-chevron-right"></i></a></li>
-                <li class="sidenav__list-item"><a href="<?= Helpers::getUrl("Mail","mail")?>"><i class="far fa-envelope"></i>MailBox<i class="fas fa-chevron-right"></i></a></li>
-                <li class="sidenav__list-item"><a href="<?= Helpers::getUrl("Parametre","parametre")?>"><i class="fas fa-cogs"></i>Réglages<i class="fas fa-chevron-right"></i></a></li>
+                <li class="sidenav__list-item"><a href="<?= Helpers::getUrl("Apparence", "apparence") ?>"><i class="fas fa-palette"></i>Apparence<i class="fas fa-chevron-right"></i></a></li>
+                <li class="sidenav__list-item"><a href="<?= Helpers::getUrl("User", "gestionuser") ?>"><i class="fas fa-user"></i>Utilisateurs<i class="fas fa-chevron-right"></i></a></li>
+                <li class="sidenav__list-item"><a href="<?= Helpers::getUrl("Mail", "mail") ?>"><i class="far fa-envelope"></i>MailBox<i class="fas fa-chevron-right"></i></a></li>
+                <li class="sidenav__list-item"><a href="<?= Helpers::getUrl("Parametre", "parametre") ?>"><i class="fas fa-cogs"></i>Réglages<i class="fas fa-chevron-right"></i></a></li>
             </ul>
 
         </aside>
         <main class="main">
             <!-- Corps du site -->
-            <?php include "views/".$this->view.".view.php"; ?>
+            <?php include "views/" . $this->view . ".view.php"; ?>
         </main>
 
         <footer class="footer">
@@ -80,6 +86,7 @@
 
     </div>
     <script src="../public/js/menu.js"></script>
-    <script src="../public/js/scriptss.js"></script>
+    <script src="../public/js/scripts.js"></script>
 </body>
+
 </html>
