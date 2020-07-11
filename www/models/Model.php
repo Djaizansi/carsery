f44f5a7717c3bd9cfe77e228c69a1e847b7cc0e9
Marwane <manbou92@hotmail.fr>
2020-07-11 15:02:55 +0200

    Ajout du model

--- www/models/Model.php
+++ www/models/Model.php
@@ -2,38 +2,53 @@
 
 namespace carsery\models;
 
-class Model
+use carsery\core\Exceptions\BDDException;
+
+interface iModel {
+    public function hydrate(array $row);
+}
+
+class Model implements iModel
 {
     public function __toArray(): array
     {
         return get_object_vars($this);
     }
 
-    /* public function hydrate(array $donnees){
-        $className = get_class($this);
+    public function hydrate(array $row)
+    {
+        $className = get_class($this);// $className = static::class
         $articleObj = new $className();
-        foreach ($donnees as $key => $value){
-        // On récupère le nom du setter correspondant à l'attribut.
-            $method = 'set'.ucfirst($key);
-        // Si le setter correspondant existe bien.
-            if (method_exists($articleObj, $method)){
-            // On appelle le setter.
-            $articleObj->$method($value);
+        foreach ($row as $key => $value) {
+        
+            $method = 'set'.ucFirst($key);
+            if (method_exists($articleObj, $method)) {
+                // Author = 4
+                if($relation = $articleObj->getRelation($key)) {
+                    // relation = User::class (App\Model\User)
+                    $tmp = new $relation();
+                    $tmp = $tmp->hydrate($row);
+                    // Maintenant on récupère notre id qui est ... la valeur actuelle de notre objet
+                    $tmp->setId($value);
+                    $articleObj->$method($tmp);
+                } else {
+                    $articleObj->$method($value);
+                }
             }
         }
-        return $articleObj;
-    } */
 
-    public function hydrate(array $donnees){
-        foreach ($donnees as $key => $value){
-        // On récupère le nom du setter correspondant à l'attribut.
-            $method = 'set'.ucfirst($key);
-        // Si le setter correspondant existe bien.
-            if (method_exists($this, $method)){
-                // On appelle le setter.
-                $this->$method($value);
-            }
-        }
-        return $this;
+        return $articleObj;
     }
+
+    public function getRelation(string $key): ?string
+    {
+        $relations = $this->initRelation();
+
+        if(isset($relations[$key]))
+            return $this->initRelation()[$key];
+
+        return null;
+    }
+
+
 }
