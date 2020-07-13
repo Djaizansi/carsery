fcc3255e661c4827d4d52751c6fa2476b753a52f
Djaizansi <youcef.jallali@gmail.com>
2020-07-13 10:54:42 +0200

    Modification MDP

--- www/core/Mail.php
+++ www/core/Mail.php
@@ -26,7 +26,7 @@ class Mail {
             $mail->Host       = 'smtp.office365.com';                    // Set the SMTP server to send through
             $mail->SMTPAuth   = true;                                   // Enable SMTP authentication
             $mail->Username   = 'service.carsery@outlook.com';                     // SMTP username
-            $mail->Password   = 'projetannuel2020';                     // SMTP password
+            $mail->Password   = 'marwaneyoucef9392';                     // SMTP password
             $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;         // Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
             $mail->Port       = 587;                                    // TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
 
