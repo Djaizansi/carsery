45a083c50004e7b9f669e6e655564785dc65b88f
lumay <mel.dispagne@gmail.com>
2020-07-15 11:31:08 +0200

    ajustement du css, rectification des appels api pour en éviter un à chaque changement de map, custom select, debut des commentaires

--- www/views/modals/contact.mod.php
+++ www/views/modals/contact.mod.php
@@ -8,70 +8,76 @@ $listContact = $contactManager->findAll();
 
 ?>
 <style>
-    /* Always set the map height explicitly to define the size of the div
-       * element that contains the map. */
+    /* Toujours définir la hauteur de la map pour l'élément qui contient la map */
     div[id^=map],
     div[id^=map] div {
-        width:800px;
-        height:800px;
+        height: 800px;
+    }
+
+    .selectmel {
+        display: block;
+        width: 100%;
+        height: 40px;
+        padding: 6px 12px;
+        font-size: 15px;
+        line-height: 1.5;
+        color: #6c6c6c;
+        background-color: #fff;
+        background-image: none;
+        border: 1px solid #ddd;
+        border-radius: 0;
+        box-shadow: none;
     }
 </style>
 <div class="container">
-    <form>
+    <form action="" method="post" id="contact">
         <span id=submit class="floating-panel"></span>
         <h1>Nous contacter</h1>
-        <label for="magasin-select">Choisissez un magasin :</label>
-        <select name="magasin" id="magasin-select" onchange="initMap()">
-            <?php foreach ($listContact as $unContact) : ?>
-                <option value="<?= $unContact->getAdresse() ?>"><?= $unContact->getAdresse() ?></option>
-            <?php endforeach ?>
-        </select>
-        <div id="map"></div>
-        <fieldset>
-            <input placeholder="Votre nom" type="text" tabindex="1" required autofocus>
-        </fieldset>
-        <fieldset>
-            <input placeholder="Votre adresse mail" type="email" tabindex="2" required>
-        </fieldset>
-        <fieldset>
-            <input placeholder="Votre numéro de téléphone" type="tel" tabindex="3" required>
-        </fieldset>
-        <fieldset>
-            <textarea placeholder="Entrez votre message ici..." tabindex="4" required></textarea>
-        </fieldset>
-        <button name="submit" type="submit" id="contact-submit" data-submit="...Sending">Envoyez !</button>
+        <h3>Choisissez un magasin : </h2>
+            <select name="magasin" id="magasin-select" class="selectmel" onchange="initMap()">
+                <?php foreach ($listContact as $unContact) : ?>
+                    <option value="<?= $unContact->getAdresse() ?>"><?= $unContact->getNom() ?>(<?= $unContact->getAdresse() ?>)</option>
+                <?php endforeach ?>
+            </select>
+            <div class="container">
+                <div id="map"></div>
+            </div>
+            <fieldset>
+                <input placeholder="Votre nom" type="text" tabindex="1" required autofocus>
+            </fieldset>
+            <fieldset>
+                <input placeholder="Votre adresse mail" type="email" tabindex="2" required>
+            </fieldset>
+            <fieldset>
+                <input placeholder="Votre numéro de téléphone" type="tel" tabindex="3" required>
+            </fieldset>
+            <fieldset>
+                <textarea placeholder="Entrez votre message ici..." tabindex="4" required></textarea>
+            </fieldset>
+            <button name="submit" type="submit" id="contact-submit" data-submit="...Sending">Envoyez !</button>
     </form>
 </div>
 <script>
     function initMap() {
-        var map = new google.maps.Map(document.getElementById('map'), {
-            zoom: 11,
-            center: {
-                lat: 40.731,
-                lng: -73.997
-            }
-        });
         var geocoder = new google.maps.Geocoder;
         var infowindow = new google.maps.InfoWindow;
         address = document.getElementById("magasin-select").value; // on récupère l'adresse sélectionné 
-        // var latlngStr = address.split(',', 2);
-        // var latlng = {
-        //     lat: parseFloat(latlngStr[0]),
-        //     lng: parseFloat(latlngStr[1])
-        // };
 
         geocoder.geocode({
             'address': address
         }, function(results, status) {
             if (status === 'OK') {
                 if (results[0]) {
-                    map.setZoom(16);
                     var latlng = results[0].geometry.location
+                    var lat = results[0].geometry.location.lat
+                    var lng = results[0].geometry.location.lng
+                    var map = new google.maps.Map(document.getElementById('map'));
                     var marker = new google.maps.Marker({
                         position: latlng,
                         map: map
                     });
                     map.setCenter(marker.getPosition())
+                    map.setZoom(16);
                 } else {
                     window.alert('No results found');
                 }
@@ -80,6 +86,7 @@ $listContact = $contactManager->findAll();
             }
         });
     }
+
     function geocodeLatLng(geocoder, map, infowindow) {
         var input = document.getElementById('latlng').value;
         var latlngStr = input.split(',', 2);
@@ -100,10 +107,10 @@ $listContact = $contactManager->findAll();
                     infowindow.setContent(results[0].formatted_address);
                     infowindow.open(map, marker);
                 } else {
-                    window.alert('No results found');
+                    window.alert('Aucun résultat trouvé avec cette adresse, merci de prévénir les administrateurs du site');
                 }
             } else {
-                window.alert('Geocoder failed due to: ' + status);
+                window.alert('Erreur geocoder code : ' + status);
             }
         });
     }
