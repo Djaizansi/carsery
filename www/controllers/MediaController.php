207c2f4a1000c98880b9a5f11863f47a4d556b40
Marwane <manbou92@hotmail.fr>
2020-07-20 11:32:53 +0200

    Ajout du controller Media

--- www/controllers/MediaController.php
+++ www/controllers/MediaController.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 
 namespace carsery\controllers;
 
@@ -7,61 +7,62 @@ use carsery\core\Helpers;
 use carsery\core\View;
 use carsery\core\Session;
 
-class MediaController {
-    public function mediaAction() 
+class MediaController
+{
+    public function mediaAction()
     {
-        if(Session::estConnecte() && Session::estAdmin()){
+        if (Session::estConnecte() && Session::estAdmin()) {
             $myView = new View("media");
-	    $alert = '';
-            if(isset($_POST['upload'])){
-                foreach($_FILES['file']['name'] as $file){
+            $alert = '';
+            if (isset($_POST['upload'])) {
+                foreach ($_FILES['file']['name'] as $file) {
                     $validExt = ['.jpg', '.jpeg', '.png'];
                     $fileExt = "." . strtolower(substr(strrchr($file, '.'), 1));
                     if (!in_array($fileExt, $validExt)) {
                         $alert = 'image';
-                        $myView->assign('alert',$alert);
+                        $myView->assign('alert', $alert);
                         break;
-                    }else {
+                    } else {
                         $file_name[] = $file;
-                        $file_store[] = "./public/images_upload/".$file;
+                        $file_store[] = "./public/images_upload/" . $file;
                     }
                 }
 
-                if($alert != 'image'){
-                    foreach($_FILES['file']['tmp_name'] as $file){
+                if ($alert != 'image') {
+                    foreach ($_FILES['file']['tmp_name'] as $file) {
                         $file_tem_loc[] = $file;
                     }
 
                     for ($i = 0; $i < count($_FILES['file']['name']); $i++) {
-                        if(move_uploaded_file($file_tem_loc[$i], $file_store[$i])){
+                        if (move_uploaded_file($file_tem_loc[$i], $file_store[$i])) {
                             $alert = 1;
-                            $myView->assign('alert',$alert);
-                        }else {
+                            $myView->assign('alert', $alert);
+                        } else {
                             $alert = 2;
-                            $myView->assign('alert',$alert);
+                            $myView->assign('alert', $alert);
                         }
                     }
-                }elseif(is_null($alert)){
-                    
+                } elseif (is_null($alert)) {
                 }
             }
-        }else {
+        } else {
             throw new RouteException("Vous devez être connecté");
         }
     }
 
     public function suppMediaAction()
     {
-        if(Session::estConnecte() && Session::estAdmin()){
-            unlink('./public/images_upload/'.$_GET['file']);
+        if (Session::estConnecte() && Session::estAdmin()) {
+            unlink('./public/images_upload/' . $_GET['file']);
             $_SESSION['success'] = 1;
-            $location = Helpers::getUrl('Media','media');
+            $location = Helpers::getUrl('Media', 'media');
             header("Location: $location");
-        }else{
+        } else {
         }
     }
 
-    public function testAction(){
+    public function testAction()
+    {
         /***************************************************
          * Only these origins are allowed to upload images *
          ***************************************************/
@@ -71,18 +72,18 @@ class MediaController {
          * Change this line to set the upload folder *
          *********************************************/
         $imageFolder = "public/images_upload/";
-        reset ($_FILES);
+        reset($_FILES);
         $temp = current($_FILES);
 
-        if (is_uploaded_file($temp['tmp_name'])){
+        if (is_uploaded_file($temp['tmp_name'])) {
             if (isset($_SERVER['HTTP_ORIGIN'])) {
-            // same-origin requests won't set an origin. If the origin is set, it must be valid.
-            if (in_array($_SERVER['HTTP_ORIGIN'], $accepted_origins)) {
-                header('Access-Control-Allow-Origin: ' . $_SERVER['HTTP_ORIGIN']);
-            } else {
-                header("HTTP/1.1 403 Origin Denied");
-                return;
-            }
+                // same-origin requests won't set an origin. If the origin is set, it must be valid.
+                if (in_array($_SERVER['HTTP_ORIGIN'], $accepted_origins)) {
+                    header('Access-Control-Allow-Origin: ' . $_SERVER['HTTP_ORIGIN']);
+                } else {
+                    header("HTTP/1.1 403 Origin Denied");
+                    return;
+                }
             }
 
             /*
@@ -116,6 +117,5 @@ class MediaController {
             // Notify editor that the upload failed
             header("HTTP/1.1 500 Server Error");
         }
-
     }
 }
