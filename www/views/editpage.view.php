9543275874a0e5888fd2e900900d69adee742921
Djaizansi <youcef.jallali@gmail.com>
2020-07-20 11:24:51 +0200

    Afficher tableau shortcode pour choisir la feature souhaitée

--- www/views/editpage.view.php
+++ www/views/editpage.view.php
@@ -10,7 +10,25 @@ $titre = $pageFound->getTitre();
         <?php if(isset($errors) && !empty($errors)): ?>
             <?= Helpers::alert('danger',$errors);?>
         <?php endif ?>
-        
+
         <h2>Editer la page : <?= $titre ?></h2>
         <?php $this->addModal("form", $configFormPage );?>
+        
+        <h2>Shortcode</h2>
+        <table id="myTable" class="display">
+            <thead>
+                <th>Shortcode</th>
+                <th>Description</th>
+                <th>Type</th>
+            </thead>
+            <tbody>
+                <?php foreach($findAllShort as $unCode): ?>
+                <tr>
+                    <td><?=$unCode->getShortcode()?></td>
+                    <td><?=$unCode->getDescription()?></td>
+                    <td><?=$unCode->getType()?></td>
+                </tr>
+                <?php endforeach ?>
+            </tbody>
+        </table>
     </div>
